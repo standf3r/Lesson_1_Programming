8a84022b1234387efd097b3a31ae60536549a6f9
standfer <vertigl@yandex.ru>
2022-03-04 00:11:35 +0800

    Homework 2: largest

---
+++ Homework_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+
+Console.Write("Самое большое число - ");
+Console.Write(max);
