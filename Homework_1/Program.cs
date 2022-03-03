4c778eff1d54ad925623410fc4647dffe5d11ad9
standfer <vertigl@yandex.ru>
2022-03-04 00:04:44 +0800

    Homework 1: More, less

---
+++ Homework_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+  Console.WriteLine("Первое число является наибольшим");  
+  Console.WriteLine(numberA);
+  Console.WriteLine("Второе число является наименьшим");
+  Console.WriteLine(numberB);
+}
+if (numberA < numberB)
+{
+  Console.WriteLine("Первое число является наименьшим");  
+  Console.WriteLine(numberA);
+  Console.WriteLine("Второе число является наибольшим");
+  Console.WriteLine(numberB);
+}
+if (numberA == numberB)
+{
+  Console.WriteLine("числа равны");    
+}
