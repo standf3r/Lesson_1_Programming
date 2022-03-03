cf04113773957a1706ec260a72ba7f4803c16504
standfer <vertigl@yandex.ru>
2022-03-03 23:42:20 +0800

    Task 3: Weekday

---
+++ Programming_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+string dayname= "";
+if (day ==1)dayname = "понедельник";
+if (day ==2)dayname = "вторник";
+if (day ==3)dayname = "среда";
+if (day ==4)dayname = "четверг";
+if (day ==5)dayname = "пятница";
+if (day ==6)dayname = "суббота";
+if (day ==7)dayname = "воскресенье";
+Console.WriteLine(dayname);
