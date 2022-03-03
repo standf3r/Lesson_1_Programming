9ffea76a6fde96f02852cb7eafcea7cc0bb5ea2e
standfer <vertigl@yandex.ru>
2022-03-04 00:23:50 +0800

    Homework 3: Even number

---
+++ Homework_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA * 2)
+{
+    Console.Write("Данное число является четным");
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.Write("Данное число не является четным");
+    Console.WriteLine(numberA);
+}
