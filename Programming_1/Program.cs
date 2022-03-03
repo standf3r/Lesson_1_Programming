6232652a876706fd0076257daf8aae92e583b183
standfer <vertigl@yandex.ru>
2022-03-03 21:26:51 +0800

    Task 1: square number

--- Programming_1/Program.cs
+++ Programming_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести квадрат числа
+Console.Write("Введите число, которое вам необходимо возвести в квадрат: ");
+
+// Конвертируем строку в целое число
+// При использовании вещественных числе используеся команда double. Для конвертирования далее применяется команда Convert.ToDouble
+int numberValue = Convert.ToInt32(Console.ReadLine());
+
+int numberA = (numberValue * numberValue);
+Console.Write("Данное число в квадрате равняется - ");
+Console.Write(numberA);
+
