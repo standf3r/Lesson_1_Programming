3b720dde787801e7186a6097c53e861073463b41
standfer <vertigl@yandex.ru>
2022-03-05 15:09:17 +0800

    Task 1: -N до N

---
+++ Programming_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа от -N до N
+
+int NumberN = 0; //-N
+int NumberN1 = 0; //N
+Console.WriteLine( "Get number -N" );
+NumberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "Get number N" );
+NumberN1 = Convert.ToInt32(Console.ReadLine());
+if (NumberN > NumberN1){
+  Console.WriteLine("Р’РІРµРґРµРЅС‹ РЅРµРІРµСЂРЅС‹Рµ РґР°РЅРЅС‹Рµ");
+
+}
+else while (NumberN <= NumberN1){
+  Console.WriteLine("" + NumberN);
+  NumberN += 1;
+}
