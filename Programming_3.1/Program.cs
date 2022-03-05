d11a835c4c9cb808fa9b8a76573568fe01e7bc83
standfer <vertigl@yandex.ru>
2022-03-05 14:49:08 +0800

    Task 3: Weekday v2

---
+++ Programming_3.1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Решение задачи с определением дня недели по вводу числа с помощью команд switch and case
+
+Console.WriteLine("Введите номер дня недели: ");
+int NumberOfWeek = Convert.ToInt32(Console.ReadLine());
+switch (NumberOfWeek){
+  case 1:
+  {
+    Console.WriteLine( "Monday");
+    break;
+  }
+  case 2:
+  {
+    Console.WriteLine( "Tuesday");
+    break;
+  }
+  case 3:
+  {
+    Console.WriteLine( "Wednesday");
+    break;
+  }
+  case 4:
+  {
+    Console.WriteLine( "Thursday");
+    break;
+  }
+  case 5:
+  {
+    Console.WriteLine( "Friday");
+    break;
+  }
+  case 6:
+  {
+    Console.WriteLine( "Saturday");
+    break;
+  }
+  case 7:
+  {
+    Console.WriteLine( "Sunday");
+    break;
+
+  }
+}
